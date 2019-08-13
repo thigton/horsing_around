5ff2d819cddd49354ad8f2426283ca4c474412e1
edgent <ed.gent@hotmail.co.uk>
2019-08-13 23:37:07 +0100

    added SQL folder and horses table creation script

---
+++ SQL/table_creation_horses.sql
@@ -0,0 +1,13 @@
+CREATE TABLE horses (
+horse_ID VARCHAR(500) PRIMARY KEY,
+race_ID VARCHAR(500) ,
+form VARCHAR(500) ,
+horse_weight VARCHAR(500) ,
+days_since_last_run INTEGER ,
+horse_age INTEGER ,
+jockey_name VARCHAR(500) ,
+jockey_form VARCHAR(500) ,
+trainer_name VARCHAR(500) ,
+stall INTEGER ,
+horse_analysis_text VARCHAR(500) ,
+horse_added_timestamp TIMESTAMP)
